20ceb42aade7df4329d2891324ff1c0f04d3d55b
remya <remyamvk@gmail.com>
2021-05-21 14:01:41 +0530

    React Router

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -1,10 +1,11 @@
 
 import Menu from './MenuComponent';
-import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 import { Component } from 'react';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class  Main extends Component {
 
@@ -16,20 +17,22 @@ class  Main extends Component {
       selectedDish: null
     };
 
-  }
-  onDishSelect(dishId){
-    this.setState({
-        selectedDish:dishId
-    });
+
 }
   render(){
+    const HomePage = () =>{
+      return(
+        <Home />
+      )
+    }
   return (
     <div>
     <Header />
-    <Menu dishes = {this.state.dishes} 
-         onClick = {(dishId) => this.onDishSelect(dishId)}/>
-    <DishDetail 
-        dish ={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+    <Switch>
+      <Route path ="/home" component ={HomePage}/>
+      <Route exact path = "/menu" component={() => <Menu dishes = {this.state.dishes}/>}/>
+      <Redirect to="/home" />
+    </Switch>
     <Footer />
   </div>
   );
