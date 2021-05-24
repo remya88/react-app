2e180d1d32bc6c2eb94cb3de533e06282610961d
remya <remyamvk@gmail.com>
2021-05-24 17:42:57 +0530

    Intro. to Redux

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -3,17 +3,22 @@ import './App.css';
 import Main from './components/MainComponent';
 import { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore(); 
 
 class  App extends Component {
 
   render(){
     return (
+      <Provider store={store}>
       <BrowserRouter>
         <div className="App">
           <Main />
         </div>
       </BrowserRouter>
-      
+      </Provider>
     );
   }
 }
